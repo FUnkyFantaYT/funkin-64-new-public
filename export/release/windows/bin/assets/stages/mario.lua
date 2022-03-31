dd1c691daa377499af4340c6a4ac1bbf1870730b
whiteninja1 <83502458+whiteninja1@users.noreply.github.com>
2022-03-31 13:24:38 +0300

    Delete export/release directory

--- export/release/windows/bin/assets/stages/mario.lua
+++
@@ -1,7 +0,0 @@
-function onCreate()
-	-- background shit
-	makeLuaSprite('bg', 'mario bg', 0, -200);
-	setScrollFactor('bg', 1, 1);
-	addLuaSprite('bg', false);
-	close(true); --For performance reasons, close this script once the stage is fully loaded, as this script won't be used anymore after loading the stage
-end
